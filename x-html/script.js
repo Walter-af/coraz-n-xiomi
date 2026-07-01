4dc6294b499fd008c3f385996692e1cd3503992d
Walter-af <walterganchozo461@gmail.com>
2026-07-01 00:43:08 -0500

    Update birthday canvas animation text and particles

--- x-html/script.js
+++ x-html/script.js
@@ -24,12 +24,12 @@ ctx.textBaseline = "middle";
 
 // Dibuja el texto base para poder leer sus pixeles
 ctx.fillText(
-    "FELIZ CUMPLEAÑOS ",
+    "MI CUMPLEAÑERA",
     canvas.width / 2,
     canvas.height / 2 + 190
 );
 ctx.fillText(
-    "MI AMOR ❤️",
+    " PRECIOSA❤️",
     canvas.width / 2,
     canvas.height / 2 + 280
 
